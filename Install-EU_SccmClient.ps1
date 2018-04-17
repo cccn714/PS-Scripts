b2026d5a9706752366496e8584a989be0991a5ed
cccn714 <31424926+cccn714@users.noreply.github.com>
2018-04-17 11:37:14 -0700

    moved files around

--- Install-EU_SccmClient.ps1
+++
@@ -1,4 +0,0 @@
-﻿New-Item -ItemType Directory -Path C:\Client
-Copy-Item -Path \\kiosk-sccm02.tillsterkiosk.com\E$\Client\* -Destination C:\Client -Recurse
-cd C:\Client
-.\ccmsetup /mp:kiosk-sccm02.tillsterkiosk.com SMSSITECODE=TK1 SMSMP=kiosk-sccm02.tillsterkiosk.com
--- Join-Domain.ps1
+++
@@ -1,2 +0,0 @@
-$name = $env:computername
-Add-Computer -DomainName Tillsterkiosk.com -ComputerName $name -Passthru -Verbose -Restart -Force

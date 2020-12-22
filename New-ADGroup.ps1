ecc4b3ac981b72cc975da8052820b9a7767beba4
CC <31424926+cccn714@users.noreply.github.com>
2020-12-21 17:38:38 -0800

    Delete New-ADGroup.ps1

--- New-ADGroup.ps1
+++
@@ -1,5 +0,0 @@
-$Path= "OU=Security Groups,DC=qa,DC=tillsterkiosk,DC=com"
-New-ADGroup -Name "LSG-Kiosk Local Admins" -GroupCategory Security -GroupScope Global -DisplayName "LSG-Kiosk Local Admins" -Path $Path
-New-ADGroup -Name "LSG-Kiosk Users" -GroupCategory Security -GroupScope Global -DisplayName "LSG-Kiosk Users" -Path $Path
-New-ADGroup -Name "LSG-SCCM Local Admins" -GroupCategory Security -GroupScope Global -DisplayName "LSG-SCCM Local Admins" -Path $Path
-New-ADGroup -Name "LSG-Server Local Admins" -GroupCategory Security -GroupScope Global -DisplayName "LSG-Server Local Admins" -Path $Path

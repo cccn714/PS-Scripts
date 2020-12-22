ad8778d16fa2d51da202aa2bdb76395bc9c20129
CC <31424926+cccn714@users.noreply.github.com>
2020-12-21 17:41:09 -0800

    Delete Set-AADServiceAccountPermissions.ps1

--- Set-AADServiceAccountPermissions.ps1
+++
@@ -1,6 +0,0 @@
-
-Import-Module "C:\Program Files\Microsoft Azure Active Directory Connect\AdSyncConfig\AdSyncConfig.psm1"
-
-Set-ADSyncPasswordHashSyncPermissions -ADConnectorAccountName SA_ADConnect -ADConnectorAccountDomain CMKTS.COM
-Set-ADSyncPasswordWritebackPermissions -ADConnectorAccountName SA_ADConnect -ADConnectorAccountDomain CMKTS.COM
-Set-ADSyncUnifiedGroupWritebackPermissions -ADConnectorAccountName SA_ADConnect -ADConnectorAccountDomain CMKTS.COM
